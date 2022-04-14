a4e599ee6ec74be35bfd9e22205fecd05257dc0f
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-15 00:52:22 +0530

    Update sticker.py

--- plugins/Don/sticker.py
+++ plugins/Don/sticker.py
@@ -14,7 +14,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import User, Message, Sticker, Document, ChatMember
 
-API_ID = os.environ('API_ID')
+API_ID = int(os.environ('API_ID')
 API_HASH = os.environ('API_HASH')
 BOT_TOKEN = os.environ('BOT_TOKEN')
 

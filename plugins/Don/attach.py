2e5f23d8045833b5b585c42cad07884d153da6e6
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-15 22:18:57 +0530

    Update attach.py

--- plugins/Don/attach.py
+++ plugins/Don/attach.py
@@ -85,3 +85,12 @@ async def modules_cb(update):
             [[InlineKeyboardButton(text="🔙 Back", callback_data="modules")]]
         )
     )
+
+
+
+@Client.on_message(filters.text & filters.private & filters.reply & filters.regex(r'https?://[^\s]+'))
+async def attach(bot, update):
+    await update.reply_text(
+        text=f"[\u2063]({update.text}){update.reply_to_message.text}",
+        reply_markup=update.reply_to_message.reply_markup
+    )

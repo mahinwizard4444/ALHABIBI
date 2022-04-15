5a889084e696eb946ddb763347e06b8e4bcbf9f0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-15 22:19:43 +0530

    Update attach.py

--- plugins/Don/attach.py
+++ plugins/Don/attach.py
@@ -5,7 +5,18 @@ from pyrogram.types import Message
 from pyrogram import Client, filters
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
+import os
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+Bot = Client(
+    "Telegram Attach Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
 
 MODULES = {
     "attach": {

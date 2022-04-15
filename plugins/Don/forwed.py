612c7be5ae39b2fab99e0d4fc1a9138a169cfa44
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-15 14:22:49 +0530

    Update forwed.py

--- plugins/Don/forwed.py
+++ plugins/Don/forwed.py
@@ -2,12 +2,7 @@ import os
 from pyrogram import Client, filters
 
 
-Bot = Client(
-     os.environ.get("SESSION_NAME", "No-Forward-Messages"),
-     bot_token = os.environ["BOT_TOKEN"],
-     api_id = int(os.environ["API_ID"]),
-     api_hash = os.environ["API_HASH"]
-)
+
 
 
 @Client.on_message(filters.forwarded)

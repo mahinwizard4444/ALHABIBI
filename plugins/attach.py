b054681e51003b11ea2057029dd62369f57ed8bc
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-16 12:31:11 +0530

    Create attach.py

---
+++ plugins/attach.py
@@ -0,0 +1,265 @@
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/Attach-Bot-V2/blob/main/LICENSE
+
+import os 
+import time
+import math
+import json
+import string 
+import random
+import traceback
+import asyncio
+import datetime
+import aiofiles
+from random import choice
+from database import Database
+import pyrogram
+from pyrogram import Client, filters 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant, UserBannedInChannel, FloodWait, InputUserDeactivated, UserIsBlocked
+from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
+
+
+Bot = Client(
+    "Attach Bot V2",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+)
+
+START_TEXT = """**Hello {} 😌
+I am a media or file in a message attach bot.**
+
+>> `I can attach photo, video, audio etc. using their public links in a message.`
+
+Made by @FayasNoushad
+"""
+HELP_TEXT = """**Hey, Follow these steps:**
+
+➠ Just send a html or markdown message
+➠ Reply a link for attaching
+
+**Tips for links**
+
+• Use @FnTelegraphBot for telegraph links of photos and videos
+• Use Telegram public channel or group message links
+• You can send any type of links for attaching
+
+**Available Commands**
+
+/start - Checking Bot Online
+/help - For more help
+/about - For more about me
+/status - For bot status
+
+Made by @FayasNoushad"""
+
+ABOUT_TEXT = """--**About Me**-- 😎
+
+🤖 Name : [Attach Bot](https://telegram.me/{})
+
+👨‍💻 Developer : [Fayas](https://github.com/FayasNoushad)
+
+📢 Channel : [Fayas Noushad](https://telegram.me/FayasNoushad)
+
+👥 Group : [Developer Team](https://telegram.me/TheDeveloperTeam)
+
+🌐 Source : [👉 Click here](https://github.com/FayasNoushad/Attach-Bot-V2)
+
+📝 Language : [Python3](https://python.org)
+
+🧰 Framework : [Pyrogram](https://pyrogram.org)
+
+📡 Server : [Heroku](https://heroku.com)"""
+
+FORCE_SUBSCRIBE_TEXT = "<code>Sorry Dear You Must Join My Updates Channel for using me 😌😉....</code>"
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⚙ Help', callback_data='help'),
+        InlineKeyboardButton('About 🔰', callback_data='about'),
+        InlineKeyboardButton('Close ✖️', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏘 Home', callback_data='home'),
+        InlineKeyboardButton('About 🔰', callback_data='about'),
+        InlineKeyboardButton('Close ✖️', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏘 Home', callback_data='home'),
+        InlineKeyboardButton('Help ⚙', callback_data='help'),
+        InlineKeyboardButton('Close ✖️', callback_data='close')
+        ]]
+    )
+
+broadcast_ids = {}
+db = Database(os.environ["DATABASE_URL"], "FnAttachBot")
+BOT_OWNER = int(os.environ["BOT_OWNER"])
+AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "")
+
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : user is blocked\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception as e:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
+
+
+@Client.on_callback_query()
+async def cb_handler(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            reply_markup=START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "helps":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            reply_markup=HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "abouts":
+        await update.message.edit_text(
+            text=ABOUT_TEXT.format((await bot.get_me()).username),
+            reply_markup=ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.message.delete()
+
+
+@Client.on_message(filters.private & filters.command(["starts"]))
+async def start(bot, update):
+    if not await db.is_user_exist(update.from_user.id):
+            await db.add_user(update.from_user.id)
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=START_BUTTONS,
+        quote=True
+    )
+
+
+@Client.on_message(filters.private & filters.command(["helps"]))
+async def help(bot, update):
+    if not await db.is_user_exist(update.from_user.id):
+            await db.add_user(update.from_user.id)
+    await update.reply_text(
+        text=HELP_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=HELP_BUTTONS,
+        quote=True
+    )
+
+
+@Client.on_message(filters.private & filters.command(["abouts"]))
+async def about(bot, update):
+    if not await db.is_user_exist(update.from_user.id):
+            await db.add_user(update.from_user.id)
+    await update.reply_text(
+        text=ABOUT_TEXT.format((await bot.get_me()).username),
+        disable_web_page_preview=True,
+        reply_markup=ABOUT_BUTTONS,
+        quote=True
+    )
+
+
+@Client.on_message(filters.private & filters.command("broadcast") & filters.user(BOT_OWNER) & filters.reply)
+async def broadcast(bot, update):
+	all_users = await db.get_all_users()
+	broadcast_msg = update.reply_to_message
+	while True:
+	    broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+	    if not broadcast_ids.get(broadcast_id):
+	        break
+	out = await update.reply_text(text=f"Broadcast Started! You will be notified with log file when all the users are notified.")
+	start_time = time.time()
+	total_users = await db.total_users_count()
+	done = 0
+	failed = 0
+	success = 0
+	broadcast_ids[broadcast_id] = dict(total = total_users, current = done, failed = failed, success = success)
+	async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
+	    async for user in all_users:
+	        sts, msg = await send_msg(user_id = int(user['id']), message = broadcast_msg)
+	        if msg is not None:
+	            await broadcast_log_file.write(msg)
+	        if sts == 200:
+	            success += 1
+	        else:
+	            failed += 1
+	        if sts == 400:
+	            await db.delete_user(user['id'])
+	        done += 1
+	        if broadcast_ids.get(broadcast_id) is None:
+	            break
+	        else:
+	            broadcast_ids[broadcast_id].update(dict(current = done, failed = failed, success = success))
+	if broadcast_ids.get(broadcast_id):
+	    broadcast_ids.pop(broadcast_id)
+	completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
+	await asyncio.sleep(3)
+	await out.delete()
+	if failed == 0:
+	    await update.reply_text(text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.", quote=True)
+	else:
+	    await update.reply_document(document='broadcast.txt', caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.")
+	os.remove('broadcast.txt')
+
+
+@Client.on_message(filters.text & filters.private & filters.reply & filters.regex(r'https?://[^\s]+'))
+async def attach(bot, update):
+    if not await db.is_user_exist(update.from_user.id):
+            await db.add_user(update.from_user.id)
+    if AUTH_CHANNEL:
+        try:
+            user = await bot.get_chat_member(UPDATE_CHANNEL, update.chat.id)
+            if user.status == "kicked":
+                await update.reply_text(text="You are banned!")
+                return
+        except UserNotParticipant:
+            await update.reply_text(
+		  text=FORCE_SUBSCRIBE_TEXT,
+		  reply_markup=InlineKeyboardMarkup(
+			  [[InlineKeyboardButton(text="😎 Join Channel 😎", url=f"https://telegram.me/{UPDATE_CHANNEL}")]]
+		  )
+	    )
+            return
+        except Exception as error:
+            print(error)
+            await update.reply_text(text="Something wrong. Contact <a href='https://telegram.me/TheFayas'>Developer</a>.", disable_web_page_preview=True)
+            return
+    await update.reply_text(
+	    text=f"[\u2063]({update.text}){update.reply_to_message.text}",
+	    reply_markup=update.reply_to_message.reply_markup
+    )
+
+
+@Client.on_message(filters.private & filters.command("status"))
+async def status(bot, update):
+    total_users = await db.total_users_count()
+    text = "**Bot Status**\n"
+    text += f"\n**Total Users:** `{total_users}`"
+    await update.reply_text(
+        text=text,
+        quote=True,
+        disable_web_page_preview=True
+    )
+
+

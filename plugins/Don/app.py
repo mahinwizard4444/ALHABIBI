4718516869a227e8d3367629220be45646f6ea14
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-16 10:40:23 +0530

    Update app.py

--- plugins/Don/app.py
+++ plugins/Don/app.py
@@ -2,6 +2,7 @@ import os
 import play_scraper
 from pyrogram import Client, filters
 from pyrogram.types import *
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent
 
 
 Bot = Client(

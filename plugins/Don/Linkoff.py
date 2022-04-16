43c0ccf8fccc300f80726cd00758214c455fb9a4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-16 10:43:04 +0530

    Update Linkoff.py

--- plugins/Don/Linkoff.py
+++ plugins/Don/Linkoff.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 
 
 
-@Client.on_message(filters.regex("http") | filters.regex("www") | filters.regex("@") | filters.regex("/start@minnal_murali_robot") | filters.regex("t.me"))
+@Client.on_message(filters.regex("Mair") | filters.regex("www") | filters.regex("@") | filters.regex("/start@minnal_murali_robot") | filters.regex("poor"))
 async def nolink(bot,message):
 	try:
 		await message.delete()
